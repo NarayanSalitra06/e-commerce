dc27ba3056ea6f16780956d7517cc97a0f65a378
NarayanSalitra06 <131778020+NarayanSalitra06@users.noreply.github.com>
2024-12-29 12:57:29 +0530

    feat: slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-<<<<<<< HEAD
 import { ProductGrid } from "./components/features/ProductGrid";
+import WomenFitness from "./components/home/WomenFitness";
 import Navbar from "./components/Navbar/Navbar";
 import PopUp from "./components/Navbar/PopUp";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     // <ProductGrid itemsPerPage={10} totalItems={100} />
     <>
       {" "}
-      <Navbar></Navbar>
+      <WomenFitness></WomenFitness>
     </>
   );
 }
 
 export default App;
-=======
-import Footer from './components/common/Footer';
-// import { ProductGrid } from './components/features/ProductGrid';
-
-function App() {
-  return (
-    <div>
-
-   
-        {/* <ProductGrid itemsPerPage={10} totalItems={100} /> */}
-       <Footer/>
-  </div>
-  );
-}
-
-export default App;
->>>>>>> origin/dd
---
+++ src/components/home/SwiperSlider.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import type { womenFitness } from "../../types/types";
+
+// Import required modules
+// import { Pagination } from "swiper/modules";
+
+const SwiperSlider: React.FC = () => {
+  const womenFitnessdata: womenFitness[] = [
+    {
+      image: "/womenpopular/img1.webp",
+      text: "BRA",
+    },
+    {
+      image: "/womenpopular/shorts2.webp",
+      text: "SPORTS",
+    },
+    {
+      image: "/womenpopular/legging2.webp",
+      text: "LEGGINGS",
+    },
+    {
+      image: "/womenpopular/hoodies.webp",
+      text: "HOODIES",
+    },
+    {
+      image: "/womenpopular/shirts.webp",
+      text: "SHIRTS",
+    },
+    {
+      image: "/womenpopular/sweatpants.webp",
+      text: "SWEATPANTS",
+    },
+  ];
+  return (
+    <>
+      <Swiper
+        slidesPerView={3.5}
+        spaceBetween={15}
+        pagination={{
+          clickable: true,
+        }}
+        // modules={[Pagination]}
+        className="mySwiper"
+      >
+        {womenFitnessdata?.map((item, index) => (
+          <SwiperSlide key={index} className="overflow-hidden relative">
+            <img
+              className="transition-all duration-1000 hover:scale-105"
+              src={item?.image}
+              alt=""
+            />
+            <div className="text-center absolute bottom-[3vw] left-[3vw] z-30">
+              <p className="font-semibold text-[27px] font-syncopate text-stone-50">
+                {item.text}
+              </p>
+              <button className="mt-2 bg-white text-black font-semibold px-6 py-2 rounded-[3vh]">
+                Shop Now
+              </button>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+};
+
+export default SwiperSlider;
