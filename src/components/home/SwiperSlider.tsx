ae492df9ced398680514e6a58d4d32e38aae338f
NarayanSalitra06 <131778020+NarayanSalitra06@users.noreply.github.com>
2024-12-30 18:28:47 +0530

    navbar responsiveness

--- src/components/home/SwiperSlider.tsx
+++ src/components/home/SwiperSlider.tsx
@@ -5,6 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+
 import { womenFitnessObject } from "../../types/types";
 
 // Import required modules
